8c9e6ce13aa3be2c141b388881fb6437c949f155
J Alex Antony Vijay <jfalexvijay@gmail.com>
2026-06-15 10:23:17 +0530

    Initial Commit

---
+++ RemoteRecruit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RemoteRecruit
+//
+//  Created by Alex Antony Vijay J on 15/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RemoteRecruit/RemoteRecruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteRecruitApp.swift
+//  RemoteRecruit
+//
+//  Created by Alex Antony Vijay J on 15/06/26.
+//
+
+import SwiftUI
+
+@main
+struct RemoteRecruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
