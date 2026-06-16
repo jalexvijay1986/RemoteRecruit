2ecc1524f1d05ceba41d462595484e50b1804445
J Alex Antony Vijay <jfalexvijay@gmail.com>
2026-06-16 21:29:19 +0530

    First commit

--- RemoteRecruit/app/RemoteRecruitApp.swift
+++ RemoteRecruit/app/RemoteRecruitApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct RemoteRecruitApp: App {
+    @StateObject private var viewModel = JobListViewModel(webService: JobsWebService(urlString: JobConstant.JobList.url))
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            JobListView(viewModel: viewModel)
         }
     }
 }
