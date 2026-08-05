ef26f253641f866797505de59c20693b3f75d892
J Alex Antony Vijay <jfalexvijay@gmail.com>
2026-08-05 10:22:55 +0530

    fix to compilation error

--- RemoteRecruit/app/RemoteRecruitApp.swift
+++ RemoteRecruit/app/RemoteRecruitApp.swift
@@ -13,7 +13,7 @@ struct RemoteRecruitApp: App {
     
     var body: some Scene {
         WindowGroup {
-            JobListView(viewModel: viewModel, error:"")
+            JobListView(viewModel: viewModel)
         }
     }
 }
